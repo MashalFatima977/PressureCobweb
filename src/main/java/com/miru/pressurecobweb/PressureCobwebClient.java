c580ab2d644b5609303d2b8a5bea9e171405bbbe
MashalFatima977 <anniedof1122@gmail.com>
2026-08-11 16:58:45 +0500

    Create PressureCobwebClient.java

---
+++ src/main/java/com/miru/pressurecobweb/PressureCobwebClient.java
@@ -0,0 +1,11 @@
+package com.miru.pressurecobweb;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PressureCobwebClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+        System.out.println("PressureCobweb Client Mod Loaded!");
+    }
+}
